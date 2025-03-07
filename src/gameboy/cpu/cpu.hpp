0dc29030ba306b9931e032f7c33c3d07ad8482a8
jjdag <131953261+jjdag@users.noreply.github.com>
2025-03-07 11:02:52 +0100

    NOP STOP LD reg1 reg2 and JR opcodes

--- src/gameboy/cpu/cpu.hpp
+++ src/gameboy/cpu/cpu.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
-#include "../logging/log/log.hpp"
-
 #include "../gameboy.hpp"
+
+// // struct instructions
+
+// struct instr {
+//     uint8_t opcode;
+//     auto* exec_func();
+//     uint8_t parameters[2];
+// };
+
 
 // Forward declaration
 class Gameboy;
@@ -18,63 +25,30 @@ class CPU {
         // Gameboy ref
         Gameboy* gameboy;
 
-        // Personnal logger ref
-        Log* logger;
-
         // Registers
-        char a, f, b, c, d, e, h, l; // 8-bit registers
-        int sp, pc; // 16-bit registers
+        uint8_t a, f, b, c, d, e, h, l; // 8-bit registers
+        uint16_t sp, pc; // 16-bit registers
 
         // Execution steps
         void fetch(); // Fetch the next instruction
+
         // void decode(); // Decode the instruction
-        // void fetchOperands(); // Fetch the operands
-        // void execute(); // Execute the instruction
+        void decodeAndExecute(uint8_t opcode);
 
-        /*
+        // void fetchOperand();
+
+        // void execute();
+
+        void NOP(); // 0x00
+        void STOP(uint8_t n8); // 0x10
+        void JRN(int8_t& e8, char& flag); // 0x20, 0x30
+        void JR(int8_t& e8, char& flag); // 0x18, 0x28
+    
+
+        void LD(uint8_t& reg1, uint8_t& reg2, uint16_t& n16); // 0x01, 0x11, 0x21, 0x31 ie load immediate 16 bit value into BC, DE, HL, SP respectivement
         
-            Instructions
 
-        */
+        // void LDBCrA(); // 0x02
 
-        // ADD, ADDC
-        void ADD(uint8_t &r1, const uint8_t &r2);
-        void ADDC(uint8_t &r1, const uint8_t &r2);
-        void ADD(uint16_t &r1, const uint16_t &r2);
 
-        // SUB, SUBC
-        void SUB(uint8_t &r1, const uint8_t &r2);
-        void SUBC(uint8_t &r1, const uint8_t &r2);
-
-        // CP
-        void CP(uint8_t &r1, const uint8_t &r2);
-
-        // INC
-        void INC(uint8_t &r1);
-        void INC(uint16_t &r1);
-
-        // DEC
-        void DEC(uint8_t &r1);
-        void DEC(uint16_t &r1);
-
-        // AND
-        void AND(uint8_t &r1, const uint8_t &r2);
-
-        // OR
-        void OR(uint8_t &r1, const uint8_t &r2);
-
-        // XOR
-        void XOR(uint8_t &r1, const uint8_t &r2);
-
-        // CCF complement carry flag
-        void CCF();
-
-        // SCF set carry flag
-        void SCF();
-
-        // DAA decimal adjust accumulator
-        void DAA();
-
-        // CPL complement accumulator
-        void CPL();
 };
